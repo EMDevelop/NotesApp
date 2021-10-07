d2f10135584f433e9adcc7ca06aa22fee57cf720
Tom Lee <tomalee2002@gmail.com>
2021-10-07 20:25:21 +0100

    adds dateTime attribute and getDateTime() to Note class

--- src/note.js
+++ src/note.js
@@ -2,5 +2,19 @@ class Note {
   constructor(title = '', body = '') {
     this.title = title;
     this.body = body;
+    this.dateTime = this.getDateTime();
+  }
+
+  getDateTime() {
+    let today = new Date();
+    let date =
+      today.getFullYear() +
+      '-' +
+      (today.getMonth() + 1) +
+      '-' +
+      today.getDate();
+    let time =
+      today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+    return date + ' ' + time;
   }
 }
