207f44e4f44e752b118aaf2869feb4161d4d0981
EMDevelop <edwardmartin.inbox@gmail.com>
2021-10-07 08:40:03 +0100

    css on click events added for all preview elements

--- src/interface.js
+++ src/interface.js
@@ -2,13 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
   // Creating Note Instance
   const notesApp = new NotesApp();
 
-  // Creating a Fake Note
-  // notesApp.newNote(
-  //   "test testy test test test",
-  //   "This is the last notes app you will ever need!"
-  // );
-  // welcome note
-
   // Fetching notes
 
   // Updating Screen
@@ -23,6 +16,35 @@ document.addEventListener('DOMContentLoaded', () => {
       preview.removeChild(preview.lastChild);
     }
 
+    const previewCardOnClickCss = (cardsArray, clickedCardID) => {
+      cardsArray.forEach((card) => {
+        if (card.id === clickedCardID) {
+          card.className = 'preview-card-clicked';
+        } else {
+          card.className = 'preview-card';
+        }
+      });
+    };
+
+    // Defining the action after clicking for preview cards
+    const previewCardOnClick = (event, id, cardsArray) => {
+      // set hidden value with current id
+      document.getElementById('current-id').innerHTML = id;
+      previewCardOnClickCss(cardsArray, id);
+    };
+
+    // Defining event listners for each preview
+    const setPreviewCardEventListeners = () => {
+      const cards = document.getElementsByClassName('preview-card');
+      const cardsArray = Array.from(cards);
+      cardsArray.forEach((card) => {
+        card.addEventListener('click', (e) => {
+          previewCardOnClick(e, card.id, cardsArray);
+        });
+      });
+    };
+
+    // Dynamically creating preview cards on screen
     const result = notesApp.getAllNotes();
     result.forEach((currentItem, index) => {
       preview.insertAdjacentHTML(
@@ -41,15 +63,16 @@ document.addEventListener('DOMContentLoaded', () => {
       noteTitle.value = currentItem.title;
       noteBody.value = currentItem.body;
     });
+    setPreviewCardEventListeners();
   };
 
   listNotesOnScreen();
 
   // Create Event Listners
   // thing = document
-  //   .getElementsByClassName("preview-card")
-  //   .addEventListener("click", (e) => {
-  //     console.log("clicked");
+  //   .getElementsByClassName('preview-card')
+  //   .addEventListener('click', (e) => {
+  //     console.log('clicked');
   //     console.log(e);
   //   });
 
