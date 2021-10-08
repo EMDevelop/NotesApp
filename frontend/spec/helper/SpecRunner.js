355b6a4390f6b9fa38e1151c03870aa951bcc318
EMDevelop <edwardmartin.inbox@gmail.com>
2021-10-08 12:45:26 +0100

    Testing Framework re-worked

--- frontend/spec/helper/SpecRunner.js
+++ frontend/spec/helper/SpecRunner.js
@@ -1,49 +1,21 @@
-class Spec {
-  // c = new Spec();
+const expect = (input) => {
+  return new Matchers(input);
+};
 
-  // beforeEach(beforFunction) {
-  //   beforFunction();
-  // }
+const describe = (label, callback) => {
+  console.log(`__________ Describe:  ${label} __________`);
+  console.log(`____________________________________________________________`);
+  console.log('');
+  callback();
+};
 
-  expect(method) {
-    // beforFunction();
-    if (method instanceof Function) {
-      this.input = method();
-    } else {
-      this.input = method;
-    }
-    return this;
-  }
+const context = (label, callback) => {
+  console.log(`::Context:: ${label}`);
+  callback();
+};
 
-  isInstanceOf(matchValue) {
-    let result = this.input instanceof matchValue;
-    let log = result
-      ? `PASS: Input: ${this.input.constructor.name} is an instance of ${matchValue.name}`
-      : `FAIL: Input: ${this.input.constructor.name} is not an instance of ${matchValue.name}`;
-    this._printResponse(log, result);
-  }
-
-  isEq(matchValue) {
-    let result = this.input === matchValue;
-    let log = result
-      ? `PASS: Input: ${this.input} is Equal to ${matchValue}`
-      : `FAIL: Input: ${this.input} is Not Equal to ${matchValue}`;
-    this._printResponse(log, result);
-  }
-
-  isNotEq(matchValue) {
-    let result = this.input !== matchValue;
-    let log = result
-      ? `PASS: Input: ${this.input} is Not Equal to ${matchValue}`
-      : `FAIL: Input: ${this.input} is Equal to ${matchValue}`;
-    this._printResponse(log, result);
-  }
-
-  _printResponse(message, result) {
-    let css = result ? 'green' : 'red';
-    console.log(`%c ${message}`, `background: ${css};`);
-  }
-}
-
-// Global test available everywhere
-test = new Spec();
+const it = (label, callback) => {
+  console.log(`::::It ${label} {`);
+  callback();
+  console.log(`}::::`);
+};
--- frontend/spec/helper/matchers.js
+++ frontend/spec/helper/matchers.js
@@ -1,11 +1,34 @@
-let thingf = '5';
+class Matchers {
+  constructor(input) {
+    this.input = input;
+  }
 
-thingf;
+  isInstanceOf(matchValue) {
+    let result = this.input instanceof matchValue;
+    let log = result
+      ? `PASS: Input: ${this.input.constructor.name} is an instance of ${matchValue.name}`
+      : `FAIL: Input: ${this.input.constructor.name} is not an instance of ${matchValue.name}`;
+    this._printResponse(log, result);
+  }
 
-const things = () => {
-  // console.log('hello');
-  return 'hello';
-};
+  isEq(matchValue) {
+    let result = this.input === matchValue;
+    let log = result
+      ? `PASS: Input: ${this.input} is Equal to ${matchValue}`
+      : `FAIL: Input: ${this.input} is Not Equal to ${matchValue}`;
+    this._printResponse(log, result);
+  }
 
-let thingthing = things();
-thingthing;
+  isNotEq(matchValue) {
+    let result = this.input !== matchValue;
+    let log = result
+      ? `PASS: Input: ${this.input} is Not Equal to ${matchValue}`
+      : `FAIL: Input: ${this.input} is Equal to ${matchValue}`;
+    this._printResponse(log, result);
+  }
+
+  _printResponse(message, result) {
+    let css = result ? 'green' : 'red';
+    console.log(`%c ${message}`, `background: ${css};`);
+  }
+}
