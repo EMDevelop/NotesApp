fc7ab42a81f6cddfe57d68a5315b20c3dcf2499f
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-10-08 13:16:59 +0100

    fixed tests to account for initial default note

--- frontend/spec/notesAppSpec.js
+++ frontend/spec/notesAppSpec.js
@@ -4,19 +4,19 @@ describe('Testing the NotesApp Object', () => {
   let noteApp = new NotesApp();
 
   it('Checks notes initiated with 1 note', () => {
-    expect(noteApp.notes.length).isEq(0);
+    expect(noteApp.notes.length).isEq(1);
   });
 
   it('notesApp creates and stores a new note', () => {
     noteApp.newNote('title', 'body');
-    expect(noteApp.notes.length).isEq(1);
+    expect(noteApp.notes.length).isEq(2);
   });
 
   it('notesApp returns a list of notes', () => {
     let noteApp1 = new NotesApp();
     noteApp1.newNote('title2', 'body2');
     noteApp1.newNote('title3', 'body3');
-    expect(noteApp1.notes.length).isEq(2);
+    expect(noteApp1.notes.length).isEq(3);
   });
 
   it('Note values saved correctly', () => {
@@ -25,7 +25,7 @@ describe('Testing the NotesApp Object', () => {
     noteApp2.newNote('title2', 'body2');
     noteApp2.newNote('title3', 'body3');
     result = noteApp2.getAllNotes();
-    expect(result[0].title).isEq('title2');
-    expect(result[0].body).isEq('body2');
+    expect(result[1].title).isEq('title2');
+    expect(result[1].body).isEq('body2');
   });
 });
